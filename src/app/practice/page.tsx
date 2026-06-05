184ae647fcacb14c3d0de145a3f46da1136ed9ae
hroh1007-arch <hroh1007@gmail.com>
2026-06-05 19:32:53 -0400

    Update page.tsx

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -901,6 +901,14 @@ export default function Home() {
                     >
                       Classrooms
                     </button>
+
+<button
+  onClick={() => (window.location.href = "/equipment")}
+  className="border px-4 py-2 rounded-lg hover:bg-gray-100"
+>
+  Equipment
+</button>
+
                   </>
                 )}
 
